b5b687b1b2fcdd7b0a9ffd2a29f45beb13305ea9
Shen Sat <shensat@shens-mbp.home>
2025-08-29 11:42:14 +0100

    Refactored into my structural style

---
+++ board/init_board.lua
@@ -0,0 +1,89 @@
+function is_valid_pos(x, y)
+  return x >= 1 and x <= GRID_W and y >= 1 and y <= GRID_H
+end
+
+function is_adjacent_to_board(x, y, board)
+  if is_valid_pos(x-1, y) and board[x-1][y] > 0 then return true end
+  if is_valid_pos(x+1, y) and board[x+1][y] > 0 then return true end
+  if is_valid_pos(x, y-1) and board[x][y-1] > 0 then return true end
+  if is_valid_pos(x, y+1) and board[x][y+1] > 0 then return true end
+  return false
+end
+
+function init_board(grid_w, grid_h, num_dominoes, domino_colors)
+  local board = {}
+  for x=1, grid_w do
+    board[x] = {}
+    for y=1, grid_h do
+      board[x][y] = 0
+    end
+  end
+
+  populate_board(board)
+
+  return board
+end
+
+function populate_board(board)
+  local success = false
+  for attempt = 1, 20 do -- MAX_ATTEMPTS
+    -- Clear board
+    for x=1, GRID_W do
+      for y=1, GRID_H do
+        board[x][y] = 0
+      end
+    end
+
+    -- 1. Place the first domino randomly
+    local start_x, start_y = flr(rnd(GRID_W)) + 1, flr(rnd(GRID_H)) + 1
+    local orient = flr(rnd(2)) -- 0 for horizontal, 1 for vertical
+    if orient == 0 and start_x == GRID_W then start_x = GRID_W - 1 end
+    if orient == 1 and start_y == GRID_H then start_y = GRID_H - 1 end
+
+    board[start_x][start_y] = 1
+    if orient == 0 then
+      board[start_x + 1][start_y] = 1
+    else
+      board[start_x][start_y + 1] = 1
+    end
+
+    -- 2. Place the remaining dominoes
+    for d = 2, NUM_DOMINOES do
+      local possible_placements = {}
+      for x=1, GRID_W do
+        for y=1, GRID_H do
+          if board[x][y] == 0 then
+            -- Check for horizontal placement
+            if is_valid_pos(x+1, y) and board[x+1][y] == 0 then
+              if is_adjacent_to_board(x, y, board) or is_adjacent_to_board(x+1, y, board) then
+                add(possible_placements, {x=x, y=y, orient=0})
+              end
+            end
+            -- Check for vertical placement
+            if is_valid_pos(x, y+1) and board[x][y+1] == 0 then
+              if is_adjacent_to_board(x, y, board) or is_adjacent_to_board(x, y+1, board) then
+                add(possible_placements, {x=x, y=y, orient=1})
+              end
+            end
+          end
+        end
+      end
+      if #possible_placements == 0 then
+        goto continue_generation_attempt
+      end
+      local chosen_placement = possible_placements[flr(rnd(#possible_placements)) + 1]
+      board[chosen_placement.x][chosen_placement.y] = d
+      if chosen_placement.orient == 0 then
+        board[chosen_placement.x+1][chosen_placement.y] = d
+      else
+        board[chosen_placement.x][chosen_placement.y+1] = d
+      end
+    end
+    success = true
+    break
+    ::continue_generation_attempt::
+  end
+  if not success then
+    print("board gen failed")
+  end
+end
---
+++ domino/init_domino.lua
@@ -0,0 +1,6 @@
+function init_domino(pip1, pip2, color)
+  -- Domino object with state and methods will go here
+  -- Placeholder for now
+  local domino = {}
+  return domino
+end
